ec5ce07d17018fc2185f4a001a58fd84e37ac895
Reka Ehreth <ehreth.reka@gmail.com>
2020-10-21 16:27:19 +0200

    small changes

--- hanoi.cpp
+++
@@ -1,6 +0,0 @@
-#include "hanoi.h"
-
-Hanoi::Hanoi()
-{
-
-}
--- hanoiwidget.h
+++ hanoiwidget.h
@@ -14,8 +14,15 @@ class HanoiWidget : public QWidget
 public:
     HanoiWidget(QWidget *parent = nullptr);
     ~HanoiWidget();
+    void init(); // sets the start state in state 
+    void final(); // checks if the current state in state is final
+    bool moveDisk(int from, int onto); // returns if the selected move can be done (i.e. if the state can be changed)
+    int getDiskNumber(); // returns the number of disks on a pole (size of state)
+    QVector<int> getDisksOnTower(); // returns a vector containing the number of disks on a pole in descending order based on size 
 
 private:
+    int size;
+    QVector<int> state;
     Ui::HanoiWidget *ui;
 };
 #endif // HANOIWIDGET_H
