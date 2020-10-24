ec9d0b76f28aaafb19dab68d3c37411f37333bfc
Reka Ehreth <ehreth.reka@gmail.com>
2020-10-24 23:45:45 +0200

    Pure torture

--- hanoiwidget.h
+++ hanoiwidget.h
@@ -12,22 +12,17 @@ class HanoiWidget : public QWidget
     Q_OBJECT
 
 private:
-    QVector<int> state;
+    
     Ui::HanoiWidget *ui;
 
 public:
     HanoiWidget(QWidget *parent = nullptr);
     ~HanoiWidget();
-    void init();                        // sets the start state in state 
-    bool final();                       // checks if the current state in state is final
-    bool moveDisk(int from, int onto);  // returns if the selected move can be done (i.e. if the state can be changed)
-    int getDiskNumber();                // returns the number of disks on a pole (size of state)
-    QVector<int> getDisksOnTower();     // returns a vector containing the number of disks on a pole in descending order based on size 
 
-private slots: 
-    void updateTowers();                // triggered when state is changed (triggered at the end of init(), succesfull moveDisk() )
-    void movingRefused();               // unsuccesfull moveDisk()
-    void gameOver();                    // after succesful moveDisk() final() returns true for the current state 
+private slots:
+    void set_on_numberSpinbox();
+    void click_on_newGameButton();
+    void sign_moveDisk();
 
 };
 #endif // HANOIWIDGET_H
